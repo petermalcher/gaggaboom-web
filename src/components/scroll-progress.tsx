eb2e8e46a836d03371faad66cfa7ecfb07ba3ba3
petermalcher <pm6077@gmail.com>
2026-07-21 19:20:09 +0200

    Scroll progress bar orange instead of yellow

--- src/components/scroll-progress.tsx
+++ src/components/scroll-progress.tsx
@@ -27,7 +27,7 @@ export function ScrollProgress() {
     <motion.div
       aria-hidden
       style={{ scaleX, opacity }}
-      className="fixed inset-x-0 top-0 z-[60] h-1 origin-left bg-acid"
+      className="fixed inset-x-0 top-0 z-[60] h-1 origin-left bg-stage-bright"
     />
   );
 }
